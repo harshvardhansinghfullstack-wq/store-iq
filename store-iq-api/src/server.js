ee5e5f27d4697e609960bf658950eca92a12dc94
harshvardhan119 <harshvardhansinghha@gmail.com>
2025-09-30 16:58:24 +0530

    resolved cors issue

--- store-iq-api/src/server.js
+++ store-iq-api/src/server.js
@@ -27,18 +27,19 @@ const app = express();
 
 // CORS configuration
 app.use(
- cors({
-   origin: "http://localhost:5173",
-   credentials: true,
-   allowedHeaders: [
-     "Origin",
-     "X-Requested-With",
-     "Content-Type",
-     "Accept",
-     "Authorization"
-   ],
- })
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+  })
 );
+
 
 // Import all routes
 const routes = require("./routes");
@@ -57,12 +58,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL|| "*", // frontend URL, e.g. http://localhost:3000
-    credentials: true, // allow cookies to be sent
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL|| "*", // frontend URL, e.g. http://localhost:3000
+//     credentials: true, // allow cookies to be sent
+//   })
+// );
 app.use(passport.initialize()); // no sessions
 
 // Mount routes
